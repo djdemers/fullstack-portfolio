a28b1f8b9fa2779e3ef4b08ad2cb6b80f112b9fb
David Joseph De Mers <djdemers@asu.edu>
2025-03-13 14:11:58 -0700

    added animation features to navbar

--- frontend-javascript/src/components/Navbar.jsx
+++ frontend-javascript/src/components/Navbar.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Ensure Bootstrap JS is loaded
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../styles/Navbar.css"; // Ensure you have this file
 
 function Navbar() {
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg navbar-dark bg-dark fixed-top ${scrolling ? "scrolled" : ""}`}>
             <div className="container-fluid">
-                {/* Brand Logo */}
                 <Link className="navbar-brand" to="/">My Portfolio</Link>
 
-                {/* Mobile Toggle Button */}
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -23,7 +38,6 @@ function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                {/* Navbar Links */}
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
