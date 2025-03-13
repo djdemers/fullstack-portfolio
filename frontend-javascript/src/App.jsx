929dddb720d5c50b3a7a81aba3474a3638d6de7b
David Joseph De Mers <djdemers@asu.edu>
2025-03-13 15:13:56 -0700

    foundations of projects component

--- frontend-javascript/src/App.jsx
+++ frontend-javascript/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<About />} />
-                <Route path="/projects" element={<h2>Projects Page (Coming Soon)</h2>} />
+                <Route path="/projects" element={<Projects />} />
                 <Route path="/resume" element={<h2>Resume Page (Coming Soon)</h2>} />
                 <Route path="/contact" element={<h2>Contact Page (Coming Soon)</h2>} />
             </Routes>
